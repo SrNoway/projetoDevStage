9a77d3b9c16c89b773f2d33ce72a2914ff09e562
Endrew Oliveira <endrewoliveiraa@gmail.com>
2025-02-20 11:09:36 -0400

    Nova página de convites adicionada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image'
 import Logo from '../assets/Logo.svg'
+
 import { ArrowRight, Mail, Radio, User } from 'lucide-react'
 import { InputField, InputIcon, InputRoot } from '@/components/Input'
 import { Button } from '@/components/Button'
 export default function Home() {
   return (
-    <main className="max-w-[1240px] mx-auto px-5 py-8 md:py-0 ">
       <div className="min-h-dvh flex flex-col justify-center gap-16">
         <div className="flex flex-col gap-8 items-center md:items-start">
           <Image src={Logo} alt='devstage' width={108.5} height={30}/>
@@ -59,6 +59,5 @@ export default function Home() {
       </div>
       </div>
 
-    </main>
   )
 }
